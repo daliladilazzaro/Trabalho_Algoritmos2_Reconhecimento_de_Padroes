34c919ce4c81555f6c82e5fb1d0aa62615d11e53
dilaz <dilaz@DESKTOP-C06I3RJ.mshome.net>
2020-09-03 08:30:10 -0300

    atualização de if else comentários

--- src/trabalho01_reconhecimento_de_padroes/Trabalho01_Reconhecimento_De_Padroes.java
+++ src/trabalho01_reconhecimento_de_padroes/Trabalho01_Reconhecimento_De_Padroes.java
@@ -1,4 +1,4 @@
-/*Entrega de trabalho
+/*Entrega de trabalho TADS - Turma B
 Nós, 
 Dalila Di Lazzaro,
 João Pedro Santos Costa
@@ -18,17 +18,30 @@ public class Trabalho01_Reconhecimento_De_Padroes {
 
     public static void main(String[] args) {
 
-        int linhas = 0, colunas = 0;
+        int linhas = 0, colunas = 0, i = 0;
         //linha de código para ler do teclado
         Scanner lerTeclado = new Scanner(System.in);
 
         //como a quantidade de linhas e colunas tem que ser >=3, fiz o do while
         do {
             //Ler a quantidade de linhas e colunas que terão na matriz
-            System.out.print("Informe a dimensão de linhas valor igual ou superior a 3: ");
+            System.out.print("Informe a dimensão de linhas: ");
             linhas = lerTeclado.nextInt();
-            System.out.print("Informe a dimensão de colunas valor igual ou superior a 3: ");
+            System.out.print("Informe a dimensão de colunas: ");
             colunas = lerTeclado.nextInt();
+            //caso a quantidade informada for <3
+            if (linhas < 3) {
+                System.out.println("\nPor favor, informe uma linha com valor igual ou maior que 3.");
+                System.out.print("Informe a dimensão de linhas valor igual ou superior a 3: ");
+                linhas = lerTeclado.nextInt();
+                i++;//aqui é pra ele continuar rodando
+            } else if (colunas < 3) {
+                System.out.println("\nPor favor, informe uma coluna com valor igual ou maior que 3.");
+                System.out.print("Informe a dimensão de colunas valor igual ou superior a 3: ");
+            colunas = lerTeclado.nextInt();
+                i++;
+            }
+            i++;
         } while (linhas < 3 && colunas < 3); //aqui, ele deve rodar enquanto for <3
 
         //código para criar matriz, juntamente com a quantidade de linhas e colunas lidas anteriormente/ ponto flutuante
@@ -40,15 +53,22 @@ public class Trabalho01_Reconhecimento_De_Padroes {
 
     }
 
+    public static float[][] funcaoReconhecimentoPadroes(float[][] matriz) {
+        funcaoImprimeMatriz(matriz);
+
+        return matriz;
+    }
+
 //função para imprimir a matriz gerada em funcaoGerarAleatoriedadeMatriz
     public static void funcaoImprimeMatriz(float[][] matriz) {
 
         //linha de código para limitar a quantidade de caracteres
         DecimalFormat valorParaDecimal = new DecimalFormat("0.00");
+        //chamando a funcaoGerarAleatoriedadeMatriz pois lá onde gerou os valores
         float matriz2[][] = funcaoGerarAleatoriedadeMatriz(matriz);
         for (int i = 0; i < matriz2.length; i++) {
             for (int j = 0; j < matriz2[i].length; j++) {
-                //aqui ele imprime e limitaa quantidade de numeros para depois da virgula
+                //aqui ele imprime e limita a quantidade de numeros para depois da virgula
                 System.out.print(valorParaDecimal.format(matriz2[i][j]) + " ");
             }
             System.out.println("");//Ele pula a linha para a matriz
