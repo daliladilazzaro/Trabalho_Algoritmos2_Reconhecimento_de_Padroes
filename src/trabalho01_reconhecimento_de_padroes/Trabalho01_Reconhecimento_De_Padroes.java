865051680e8b226a1c554d249bafe96194d6a9c3
João Pedro <https://github.com/Joaoojohn>
2020-09-04 16:58:15 -0300

    função ler arquivo

--- src/trabalho01_reconhecimento_de_padroes/Trabalho01_Reconhecimento_De_Padroes.java
+++ src/trabalho01_reconhecimento_de_padroes/Trabalho01_Reconhecimento_De_Padroes.java
@@ -10,91 +10,59 @@ não disponibilizamos nossas respostas para colegas externos à equipe e
 não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar outros.*/
 package trabalho01_reconhecimento_de_padroes;
 
-import java.text.DecimalFormat;
-import java.util.Scanner;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class Trabalho01_Reconhecimento_De_Padroes {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) throws Exception{
 
-        int linhas = 0, colunas = 0, i = 0;
-        //linha de código para ler do teclado
-        Scanner lerTeclado = new Scanner(System.in);
-
-        //como a quantidade de linhas e colunas tem que ser >=3, fiz o do while
-        do {
-            //Ler a quantidade de linhas e colunas que terão na matriz
-            System.out.print("Informe a dimensão de linhas: ");
-            linhas = lerTeclado.nextInt();
-            System.out.print("Informe a dimensão de colunas: ");
-            colunas = lerTeclado.nextInt();
-            //caso a quantidade informada for <3
-            if (linhas < 3) {
-                System.out.println("\nPor favor, informe uma linha com valor igual ou maior que 3.");
-                System.out.print("Informe a dimensão de linhas valor igual ou superior a 3: ");
-                linhas = lerTeclado.nextInt();
-                i++;//aqui é pra ele continuar rodando
-            } else if (colunas < 3) {
-                System.out.println("\nPor favor, informe uma coluna com valor igual ou maior que 3.");
-                System.out.print("Informe a dimensão de colunas valor igual ou superior a 3: ");
-                colunas = lerTeclado.nextInt();
-                i++;
+        float [][] matriz = LerArquivo("matriz.txt");   
+     
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(matriz[i] [j] + " ");
             }
-            i++;
-        } while (linhas < 3 && colunas < 3); //aqui, ele deve rodar enquanto for <3
-
-        //código para criar matriz, juntamente com a quantidade de linhas e colunas lidas anteriormente/ ponto flutuante
-        float matriz[][] = new float[linhas][colunas];
-
-        //chamando as funções para poder imprimir na tela
-        funcaoGerarAleatoriedadeMatriz(matriz);
-        funcaoImprimeMatriz(matriz);//void-não tem retorno
+            
+        }
+        
+        System.out.println();
 
     }
 
     public static float[][] funcaoReconhecimentoPadroes(float[][] matriz) {
-        funcaoImprimeMatriz(matriz);
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-
-            }
-        }
-
-        return matriz;
+        return null;
     }
 
-//função para imprimir a matriz gerada em funcaoGerarAleatoriedadeMatriz
-    public static void funcaoImprimeMatriz(float[][] matriz) {
-
-        //linha de código para limitar a quantidade de caracteres
-        DecimalFormat valorParaDecimal = new DecimalFormat("0.0");
-
-        //chamando a funcaoGerarAleatoriedadeMatriz pois lá onde gerou os valores
-        float matriz2[][] = funcaoGerarAleatoriedadeMatriz(matriz);
-
-        for (int i = 0; i < matriz2.length; i++) {
-            for (int j = 0; j < matriz2[i].length; j++) {
-                //aqui ele imprime e limita a quantidade de numeros para depois da virgula
-                System.out.print(valorParaDecimal.format(matriz2[i][j]) + " ");
+     public static float[][] LerArquivo(String Arquivo) throws Exception
+    {
+        FileReader arquivoGravado = new FileReader(Arquivo);
+        
+        BufferedReader ler = new BufferedReader(arquivoGravado);
+        
+        String linha1 = ler.readLine();
+        String linha2 = ler.readLine();
+        
+        ler.close();
+        
+        double linhas = Double.parseDouble(linha1);
+        double colunas = Double.parseDouble(linha2); 
+        
+        double[][] m = new double[linhas][colunas];
+        
+        String[] dados = linha1.split(" ");
+        
+        for (int i = 0; i < m.length; i++) 
+        {
+            for (int j = 0; j < m.length; j++) 
+            {
+               // m[i][j]= Float.parseFloat(dados[i][j]);
             }
-            System.out.println("");//Ele pula a linha para a matriz
+            
         }
-        // return matriz2;
+        
+        return m;
     }
 
-//essa função é para gerar os valores aleatórios da Matriz a partir de Random
-    public static float[][] funcaoGerarAleatoriedadeMatriz(float[][] matriz) {
-        //Código Random para gerar valores aleatórios, incluindo valor 1 para gerar de 0.0 até 1.0
-        Random numerosAleatorios = new Random(1);
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                //inclusão de valor no random, pra poder gerar dentro desse valor, e +1 pra não gerar velor negativo
-                matriz[i][j] = numerosAleatorios.nextFloat();
-            }
-        }
-
-        return matriz;
-    }
 }
